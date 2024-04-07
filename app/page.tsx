22d19e86dfd2622bf6db7510f76ee5a95d844a43
ShrutiSemwal <semwalshruti450@gmail.com>
2024-04-07 19:59:08 +0530

    Add Database Setup, Product Details, Cron job

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import ProductCard from "@/components/ProductCard";
 import Searchbar from "@/components/Searchbar";
+import { getAllProducts } from "@/lib/actions";
 import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
-        <div className="flex max-xl: flex-col gap-16">
+        <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping Starts Here:
@@ -33,8 +37,8 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {["Apple Iphone 15", "Book", "Sneakers"].map((product) => (
-            <div> {product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
